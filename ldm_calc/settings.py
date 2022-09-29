c53ce1e0c3061759f14f04c611a79342f3f23903
User <samsebedrug@gmail.com>
2022-09-29 23:48:16 +0300

    Amended settings.py

--- ldm_calc/settings.py
+++ ldm_calc/settings.py
@@ -27,9 +27,9 @@ IS_HEROKU = "DYNO" in os.environ
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "django-insecure-s2n)r1#ivhfxsz!%rk91#th1g1txb^y6w7z7un%i^c4h&=1o0-")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"] #".herokuapp.com"
+ALLOWED_HOSTS = ["https://ldm-calculator.herokuapp.com/"] #".herokuapp.com"
 
 
 # Application definition
